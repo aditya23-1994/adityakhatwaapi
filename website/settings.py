a78032eb59b59969e039296dafc34f41ef2c5562
aditya23-1994 <adityakhatwa@outlook.com>
2020-11-01 11:16:47 +0530

    added the requirements and django-heroku libraray

--- website/settings.py
+++ website/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import django_heroku
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -139,5 +140,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-
+django_heroku.settings(locals())
 
